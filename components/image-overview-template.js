27ff1b8c47a555d381306286ccc61143659c1458
erfan gharib <erfangharib5@gmail.com>
2021-12-06 14:06:51 +0330

    make image-overview-template dynamic

--- components/image-overview-template.js
+++ components/image-overview-template.js
@@ -1,16 +1,16 @@
-import image_data_$data, {image_overview_template_$d_html} from "../js/variable.js";
+import image_data_$data from "../js/variable.js";
 // make image overview template dynamic
-image_data_$data.then((data)=> {
+let image_overview_template_$d_html = image_data_$data.then((data) => {
     return (`
         <figure class="img-box mb-6 w-74 relative">
             <img
                 class="art-img"
-                src="${data[img_data_num].src}" 
-                alt="${data[img_data_num].image_name}"
+                src="${data[0].src}" 
+                alt="${data[0].image_name}"
             >
             <figcaption class="art/author-name w-74">
-                <h1 class="capitalize font-bold text-5md">${data[img_data_num].image_name}</h1>
-                <h3 class="capitalize">${data[img_data_num].author_name}</h3>
+                <h1 class="capitalize font-bold text-5md">${data[0].image_name}</h1>
+                <h3 class="capitalize">${data[0].author_name}</h3>
             </figcaption>
         </figure>
     `)
